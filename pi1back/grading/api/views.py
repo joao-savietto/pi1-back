deccbf1acb7f594addbb3a27a2c5c04db4acc47b
João Savietto <Joo Savietto>
2024-09-30 20:07:20 -0300

    feat: add permission classes to GradingViewSet

--- pi1back/grading/api/views.py
+++ pi1back/grading/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from ..models import Grading
 from .serializers import GradingSerializer
+from pi1back.shared.permissions import IsSuperUserOrReadOnly, IsProfessorOrReadOnly
 
 class GradingViewSet(viewsets.ModelViewSet):
     queryset = Grading.objects.all()
     serializer_class = GradingSerializer
+    permission_classes = [IsSuperUserOrReadOnly | IsProfessorOrReadOnly]
