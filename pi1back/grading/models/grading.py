84c23cfc5074566b7d176b052a346561ab74e5f2
João Savietto <joao.savietto@youtan.com.br>
2024-10-03 18:33:15 -0300

    fix: duplicate signal removed

--- pi1back/grading/models/grading.py
+++ pi1back/grading/models/grading.py
@@ -24,15 +24,3 @@ class Grading(models.Model):
         self.final_grade = self.first_exam + self.second_exam + self.third_exam + self.practice_exam
         super(Grading, self).save(*args, **kwargs)
 
-@receiver(post_save, sender=Grading)
-def create_occurrence_for_insufficient_grade(sender, instance, **kwargs):
-    # Check if all grades are posted
-    if instance.first_exam is not None and instance.second_exam is not None and instance.third_exam is not None and instance.practice_exam is not None:
-        if instance.final_grade < 5.0:
-            description = f"O aluno não obteve nota suficiente para passar o trimestre na disciplina {instance.subject.name}."
-            Occurrence.objects.create(
-                created_by=instance.user,
-                student=instance.user,
-                occurrence_type=Occurrence.NOTA_INSUFICIENTE,
-                description=description
-            )
